e9d77c6e9870577b33cc6542149866888449caf2
Rushi-Sh <rushi2706@gmail.com>
2025-07-27 15:39:26 +0530

    server updated

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from graph.graph_1 import competitor_graph
-from tools.twilio_messenger import send_sms,send_whatsapp
+from tools.twilio_messenger import send_sms, send_whatsapp
 import uvicorn
-from fastapi.middleware.cors import CORSMiddleware  
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class productInput(BaseModel):
@@ -37,23 +37,25 @@ async def competitor_node(input_data: productInput):
         result = competitor_graph(input_data.product)
         return {
             "refined_product": result["refined_product"],
-            "analysis": result["analysis"]      
+            "analysis": result["analysis"]
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
 @app.post("/twilio_messenger")
 async def twilio_messenger(input_data: twilioInput):
     try:
         if input_data.type == "whatsapp":
-            send_whatsapp(input_data.to, input_data.message)
-            return {"status": "Whatsapp Message sent successfully"}
+            sid = send_whatsapp(input_data.message, input_data.to)
+            return {"status": "Whatsapp Message sent successfully", "sid": sid}
         elif input_data.type == "sms":
-            send_sms(input_data.to, input_data.message)
-            return {"status": "SMS Message sent successfully"}
+            sid = send_sms(input_data.message, input_data.to)
+            return {"status": "SMS Message sent successfully", "sid": sid}
+        else:
+            raise HTTPException(status_code=400, detail="Invalid message type")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
+        print(f"Twilio error: {e}")
+        raise HTTPException(status_code=500, detail=f"Twilio error: {e}")
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
